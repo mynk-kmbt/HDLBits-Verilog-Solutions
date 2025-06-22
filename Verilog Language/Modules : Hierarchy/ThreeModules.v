a404ec8277929f913967de88422558fa13703e01
MAYANK KUMBHAT <143329937+mynk-kmbt@users.noreply.github.com>
2025-06-22 14:17:45 +0530

    Create ThreeModules.v

---
+++ Verilog Language/Modules : Hierarchy/ThreeModules.v
@@ -0,0 +1,11 @@
+module top_module ( input clk, input d, output q );
+	
+    wire q1, q2, q3;
+    
+    my_dff inst1 (clk, d, q1);
+    my_dff inst2 (clk, q1, q2);
+    my_dff inst3 (clk, q2, q3);
+    
+    assign q=q3;
+    
+endmodule
