a12905c98d8fcc5b8ae1493e8aeb85a998172986
MAYANK KUMBHAT <143329937+mynk-kmbt@users.noreply.github.com>
2025-06-23 19:48:58 +0530

    Create 4variable2.v

---
+++ Circuits/Combinational Logic/Karnaugh Map to Circuit/4variable2.v
@@ -0,0 +1,12 @@
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out  ); 
+    
+    // assign out=a| (~b&c);
+    
+    assign out = (a|~b)&(a|c); 
+
+endmodule
