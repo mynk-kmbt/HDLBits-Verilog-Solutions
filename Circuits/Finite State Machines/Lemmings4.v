c783e9e9b6d127aba50427a7db9a53e71d7bbb34
Mayank Kumbhat <143329937+mynk-kmbt@users.noreply.github.com>
2025-07-03 21:51:48 +0530

    Create Lemmings4.v

---
+++ Circuits/Finite State Machines/Lemmings4.v
@@ -0,0 +1,70 @@
+module top_module(
+    input clk,
+    input areset,    // Freshly brainwashed Lemmings walk left.
+    input bump_left,
+    input bump_right,
+    input ground,
+    input dig,
+    output walk_left,
+    output walk_right,
+    output aaah,
+    output digging ); 
+    
+    parameter [2:0] left=3'b00, right=3'b01, leftgnd=3'b10, rightgnd=3'b11, digleft=3'd4, digright=3'd5, splash=3'd6;
+    
+    reg [2:0] state, next_state;
+    
+    reg [4:0] counter;
+    
+    always@ (posedge clk)
+        if (~((state==leftgnd)|(state==rightgnd)))
+            counter<=0;
+    else if (counter==5'd31)
+            counter<=counter;
+    else
+        counter<=counter+1'b1;
+    
+    always@ (*)
+        case(state)
+            left: 
+                	begin
+                        if (dig&ground)
+                            next_state=digleft;                          
+                        else if (ground)
+                            if (bump_left) next_state=right; else next_state=left;
+                        else
+                            next_state=leftgnd;
+                    end
+            right:
+                	begin
+                        if (dig&ground)
+                            next_state=digright;                          
+                        else if (ground)
+                            if (bump_right) next_state=left; else next_state=right;
+                        else
+                            next_state=rightgnd;
+                    end
+            leftgnd: 	if (counter>=5'd20&ground) next_state=splash;
+                		else if (ground) next_state=left; else next_state=leftgnd;
+            rightgnd:if (counter>=5'd20&ground) next_state=splash; 
+                	 else if (ground) next_state=right; else next_state=rightgnd;
+            
+            digleft: if (ground) next_state=digleft; else next_state=leftgnd;
+            digright: if (ground) next_state=digright; else next_state=rightgnd;
+            
+            splash: next_state=splash;
+        endcase
+    
+    always@(posedge clk, posedge areset)
+        if (areset)
+            state<=left;
+    	else
+            state<=next_state;
+    
+    assign walk_left=(state==left);
+    assign walk_right=(state==right);
+    
+    assign aaah=(state==leftgnd)|(state==rightgnd);
+    assign digging= (state==digleft)|(state==digright);
+
+endmodule
