98b96e8a0e7bebe6b66d096582059c8a6a62b734
Mayank Kumbhat <143329937+mynk-kmbt@users.noreply.github.com>
2025-07-03 20:12:03 +0530

    Create SimpleFSM3synchronusReset.v

---
+++ Circuits/Finite State Machines/SimpleFSM3synchronusReset.v
@@ -0,0 +1,24 @@
+module top_module(
+    input clk,
+    input in,
+    input reset,
+    output out); 
+
+
+    parameter[1:0] A=2'd0, B=2'd1, C=2'd2, D=2'd3;
+    
+    reg [1:0] state, next_state;
+    // State transition logic
+    assign next_state[0]=in;
+    assign next_state[1]= (~in&state[0])| (in&state[1]&~state[0]);
+    
+    // State flip-flops with asynchronous reset
+    always@ (posedge clk)
+        if (reset)
+            state<=A;
+    	else
+            state<=next_state;
+    // Output logic
+    assign out=(state==D);
+
+endmodule
