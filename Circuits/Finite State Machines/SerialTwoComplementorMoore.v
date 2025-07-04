8ce1b6cf57d2365ceb18faf2880ee81d63e64332
Mayank Kumbhat <143329937+mynk-kmbt@users.noreply.github.com>
2025-07-04 11:41:47 +0530

    Update SerialTwoComplementorMoore.v

--- Circuits/Finite State Machines/SerialTwoComplementorMoore.v
+++ Circuits/Finite State Machines/SerialTwoComplementorMoore.v
@@ -4,15 +4,14 @@ module top_module (
     input x,
     output z
 ); 
-    parameter A=0, B=1, C=2, D=3;
+    parameter A=0, B=1, C=2;
     reg [1:0] state, next_state;
     
     always @ (*)
         case(state)
             A: if (x) next_state<=B; else next_state<=A;
-            B: if (x) next_state<=C; else next_state<=D;
-            C: if (x) next_state<=C; else next_state<=D;
-            D: if (x) next_state<=C; else next_state<=D;
+            B: if (x) next_state<=C; else next_state<=B;
+            C: if (x) next_state<=C; else next_state<=B;
         endcase
     
     always@(posedge clk, posedge areset)
@@ -21,10 +20,6 @@ module top_module (
     	else
             state<=next_state;
     
-    assign z=( (state==B )| (state==D));
+    assign z=(state==B );
 
-  //can be implemented in three states
-  //  C: next_state<=C;
-  // assign z=(state==C )?~x:x; 
-  // basicly we combined the C and D and instructed z to invert .
 endmodule
