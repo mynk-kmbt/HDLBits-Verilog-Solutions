0c790e072364d45bf4ea262e59846d58f4a30c97
Mayank Kumbhat <143329937+mynk-kmbt@users.noreply.github.com>
2025-07-06 00:25:58 +0530

    Update Mux.v

--- Verification: Reading Simulations/Finding bugs in code/Mux.v
+++ Verification: Reading Simulations/Finding bugs in code/Mux.v
@@ -7,4 +7,7 @@ module top_module (
   assign out = ({8{sel}} & a) | (~{8{sel}} & b); // convert sel from 1 bit to 8 bits then apply bitwise operator
   // Also for some reason they expect b when sel =0 and a when sel=1
 
+  // this will also work
+  // assign out = sel ? a : b;
+
 endmodule
