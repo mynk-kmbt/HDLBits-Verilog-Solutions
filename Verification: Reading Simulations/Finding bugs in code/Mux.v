a4546404095420ba64f9ed669930607f74f8657a
Mayank Kumbhat <143329937+mynk-kmbt@users.noreply.github.com>
2025-07-06 00:24:39 +0530

    Create Mux.v

---
+++ Verification: Reading Simulations/Finding bugs in code/Mux.v
@@ -0,0 +1,10 @@
+module top_module (
+    input sel,
+    input [7:0] a,
+    input [7:0] b,
+  output [7:0] out  ); // out should also be 8 bits
+
+  assign out = ({8{sel}} & a) | (~{8{sel}} & b); // convert sel from 1 bit to 8 bits then apply bitwise operator
+  // Also for some reason they expect b when sel =0 and a when sel=1
+
+endmodule
