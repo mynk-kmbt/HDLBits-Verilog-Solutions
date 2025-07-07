bf983871dab943092a3719a8963a0a46d6e4ca9d
Mayank Kumbhat <143329937+mynk-kmbt@users.noreply.github.com>
2025-07-08 01:02:29 +0530

    Create Clock.v

---
+++ Verification : Writing testbenches/Clock.v
@@ -0,0 +1,14 @@
+module top_module ( );
+    
+    reg clk;
+    
+    dut uut (.clk(clk));
+    
+    initial 
+        begin
+            clk=0;
+            forever #5 clk=~clk;
+        end
+
+    
+endmodule
