1fde21fa63019f4582ccb2d8da34b0328353339c
Mayank Kumbhat <143329937+mynk-kmbt@users.noreply.github.com>
2025-07-14 01:11:34 +0530

    Create Q1.v

---
+++ CS450/Q1.v
@@ -0,0 +1,16 @@
+module top_module(
+	input clk, 
+	input load, 
+	input [9:0] data, 
+	output tc
+);
+    reg [9:0] counter;
+    
+    always@(posedge clk)
+        if (load) counter<=data;
+    else if (counter==0) counter<=0;
+    else counter<=counter-1;
+    
+    assign tc=(counter==0);
+
+endmodule
