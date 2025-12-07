5747a6685a40bc7d64f66f295f4f2632673abf0b
Mayank Kumbhat <143329937+mynk-kmbt@users.noreply.github.com>
2025-12-07 22:35:44 +0530

    Update Generate100digitBCDadder.v

--- Verilog Language/More Verilog Features/Generate100digitBCDadder.v
+++ Verilog Language/More Verilog Features/Generate100digitBCDadder.v
@@ -3,19 +3,16 @@ module top_module(
     input cin,
     output cout,
     output [399:0] sum );
-    
-	wire [99:0] carry;
-    
-    bcd_fadd inst1 (a[3:0], b[3:0], cin,carry, sum[3:0] );
-    
+	
+    wire [99:-1] carry;
+    assign carry[-1]=cin;
+    assign cout=carry[99];
     genvar i;
     generate
-        for(i=1;i<100;i=i+1)begin : gen_blck
-            bcd_fadd inst (a[4*i+3:4*i], b[4*i+3:4*i], carry[i-1],carry[i], sum[4*i+3:4*i] );
-        end
+        for (i=0; i<100; i++)
+            begin: BCD_ADDERS
+                bcd_fadd inst (a[4*i +3: 4*i],b[4*i +3: 4*i],carry[i-1],carry[i],sum[4*i +3: 4*i]);
+            end
     endgenerate
     
-    assign cout=carry[99];
-    
-
 endmodule
